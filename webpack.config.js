fad76255b671219e5c6b7b165002308a6e34501c
Evans Owino <evansowino@hotmail.com>
2018-09-04 21:11:59 +0100

    config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
-const path = require('path');
+const { resolve, join } = require('path')
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const outputFolder = 'dist';
+const path = {
+  dist: resolve(__dirname, 'dist'),
+  src: resolve(__dirname, 'src'),
+}
 
-module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
-  mode: 'production',
+module.exports = ({ production = false } = {}) => ({
+  entry: ['@babel/polyfill', join(path.src, 'index.js')],
+  mode: production ? 'production' : 'development',
+  devtool: production ? false : 'inline-source-map',
   output: {
-    path: path.resolve(__dirname, outputFolder),
+    path: path.dist,
     filename: 'bundle.js'
   },
   module: {
@@ -25,5 +29,8 @@ module.exports = {
       }
     ]
   },
-  plugins: [new CleanWebpackPlugin([outputFolder]), new HtmlWebpackPlugin()]
-};
+  plugins: [
+    new CleanWebpackPlugin(path.dist), 
+    new HtmlWebpackPlugin()
+  ],
+});
